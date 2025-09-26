eb5c8e5aeee1b882af52bce50a017dc1b90e2fec
trungducnguyen4 <nguyentrungduc.forwork@gmail.com>
2025-09-27 02:21:06 +0700

    add Classroom services

---
+++ ClassroomService/src/main/java/org/tduc/classroomservice/ClassroomServiceApplication.java
@@ -0,0 +1,13 @@
+package org.tduc.classroomservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassroomServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassroomServiceApplication.class, args);
+	}
+
+}
---
+++ ClassroomService/src/test/java/org/tduc/classroomservice/ClassroomServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tduc.classroomservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassroomServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
