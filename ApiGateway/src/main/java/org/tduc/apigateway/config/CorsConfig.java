cf43344187b18bcbe026707a9abc4f7a89859abc
trungducnguyen4 <nguyentrungduc.forwork@gmail.com>
2026-01-23 14:43:36 +0700

    allocate new ip

--- ApiGateway/src/main/java/org/tduc/apigateway/config/CorsConfig.java
+++ ApiGateway/src/main/java/org/tduc/apigateway/config/CorsConfig.java
@@ -21,8 +21,8 @@ public class CorsConfig {
             // Development - localhost
             "http://localhost:*", 
             "http://127.0.0.1:*",
-            "http://3.25.204.17",
-            "http://3.25.204.17:*",
+            "http://3.26.190.126",
+            "http://3.26.190.126:*",
             "https://localhost:*",
             "https://127.0.0.1:*",
             
